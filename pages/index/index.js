e035bd388633b2e85ef693743c5589ff585a6f21
Rui2017s <191244181@qq.com>
2019-01-19 17:53:39 +0800

    icon,layout,transition,checkbox-

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,24 @@ const app = getApp()
 
 Page({
   data: {
-
-  }, 
+    show: false,
+    checked: true,
+    icon: {
+      normal: '//img.yzcdn.cn/icon-normal.png',
+      active: '//img.yzcdn.cn/icon-active.png'
+    }
+  },
   onLoad: function () {
-    
+
+  },
+  onClose() {
+    this.setDate({
+      show: false
+    })
+  },
+  onChange(event){
+    this.setDate({
+      checked:event.detail
+    })
   }
 })
