f39c3a43b05d6c655311a899d8306b3929d62885
Rui2017s <191244181@qq.com>
2019-01-23 18:01:46 +0800

    checkbox,filed,datepicker

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,9 +7,17 @@ Page({
     show: false,
     checked: true,
     icon: {
-      normal: '//img.yzcdn.cn/icon-normal.png',
-      active: '//img.yzcdn.cn/icon-active.png'
-    }
+      normal: '../../assets/images/1169251.png',
+      active: '../../assets/images/1131981.png'
+    },
+    list:['a','b','c'],
+    result:['a','b'],
+    minHour:10,
+    maxHour:20,
+    minDate:new Date().getTime(),
+    maxDate:new Date(2019,10,1).getTime(),
+    currentDate:new Date().getTime(),
+    value:''
   },
   onLoad: function () {
 
@@ -20,8 +28,30 @@ Page({
     })
   },
   onChange(event){
-    this.setDate({
+    this.setData({
       checked:event.detail
     })
+  },
+  onChange1(event) {
+    this.setData({
+      result: event.detail
+    })
+  },
+  toggle(event){
+    const {name}=event.currentTarget.dataset;
+    const checkbox=this.selectComponent(`.checkboxes-${name}`);
+    checkbox.toggle();
+  },
+  noop(){},
+  onChange3(event){
+    this.setData({
+      currentDate:event.detail.value
+    })
+  },
+  onChange4(event) {
+    this.setData({
+      value:event.detail
+    })
+    console.log(event.detail)
   }
 })
